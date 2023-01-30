9cc91e53216c5fa85c8662c6c734fb070614f94e
Archi903 <artpele@yandex.ru>
2023-01-30 15:44:03 +0300

    Задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Введите текст");
+        Scanner key = new Scanner(System.in);
+        String words = key.nextLine();
+        String[] list = words.split(" ");
+        Map<String, Integer> unique = new TreeMap<>();
+        for (String word : list) {
+            if (!unique.containsKey(word)) {
+                unique.put(word, 1);
+            } else {
+                unique.put(word, unique.get(word) + 1);
+            }
+        }
+        System.out.println("В тексте " + unique.size() + " слов \nТоп 10:" );
+        List<Map.Entry<String, Integer>> last = unique.entrySet().stream().sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue())).collect(Collectors.toList());
+        for (Map.Entry<String, Integer> stringIntegerEntry : last) {
+            System.out.println(stringIntegerEntry);
+        }
     }
-}
+    }
